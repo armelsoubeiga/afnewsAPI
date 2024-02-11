6a352f66f679f8a18c6a323af29f8c2e324592e7
armelsoubeiga <armelsoubeiga93@gmail.com>
2024-02-11 14:54:46 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,25 +1,4 @@
-library(DBI)
-library(RPostgres)
 
-db_name <- Sys.getenv("DB_NAME")
-db_host <- Sys.getenv("DB_HOST")
-db_port <- Sys.getenv("DB_PORT")
-db_user <- Sys.getenv("DB_USER")
-db_pass <- Sys.getenv("DB_PASS")
-
-# Connexion
-getcon <- tryCatch({
-  drv <- RPostgres::Postgres()
-  con <- dbConnect(drv,
-                   dbname = db_name,
-                   host = db_host,
-                   port = db_port,
-                   user = db_user,
-                   password = db_pass)
-  TRUE},
-  error=function(cond) {
-    FALSE
-  })
 
 # run scraping
 x <- data.frame(col=rnorm(1:10))
