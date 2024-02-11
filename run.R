80719d6a752bde003e9f873da4df167377a22401
armelsoubeiga <armelsoubeiga93@gmail.com>
2024-02-11 01:33:33 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,15 +1,21 @@
 library(DBI)
 library(RPostgres)
 
+db_name <- Sys.getenv("DB_NAME")
+db_host <- Sys.getenv("DB_HOST")
+db_port <- Sys.getenv("DB_PORT")
+db_user <- Sys.getenv("DB_USER")
+db_pass <- Sys.getenv("DB_PASS")
+
 # Connexion
 getcon <- tryCatch({
   drv <- RPostgres::Postgres()
   con <- dbConnect(drv,
-                   dbname = Sys.getenv("DSN_DATABASE"),
-                   host = Sys.getenv("DSN_HOSTNAME"),
-                   port = 5432,
-                   user = Sys.getenv("DSN_UID"),
-                   password = Sys.getenv("DSN_PWD"))
+                   dbname = db_name,
+                   host = db_host,
+                   port = db_port,
+                   user = db_user,
+                   password = db_pass)
   TRUE},
   error=function(cond) {
     FALSE
