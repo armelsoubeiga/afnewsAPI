a9dbd49677280db4c5ac4c01afd51834e1658e98
armelsoubeiga <armelsoubeiga93@gmail.com>
2024-02-13 13:06:53 +0100

    Update run.R

--- run.R
+++ run.R
@@ -6,32 +6,16 @@ db_host <- Sys.getenv("DB_HOST")
 db_port <- Sys.getenv("DB_PORT")
 db_user <- Sys.getenv("DB_USER")
 db_pass <- Sys.getenv("DB_PASS")
-print(db_pass)
+
 
 # Connexion
-getcon <- tryCatch({
-  drv <- RPostgres::Postgres()
-  con <- dbConnect(drv,
-                   dbname = db_name,
-                   host = db_host,
-                   port = db_port,
-                   user = db_user,
-                   password = db_pass)
-  TRUE},
-  error=function(cond) {
-    FALSE
-  })
+drv <- RPostgres::Postgres()
+con <- dbConnect(drv,
+      dbname = db_name,
+      host = db_host,
+      port = db_port,
+      user = db_user,
+  password = db_pass)
 
-# run scraping
-x <- data.frame(col=rnorm(1:10))
-head(x)
-
-# Creation et enregistrement
-if(getcon){
-  if(dbExistsTable(con, "afnewsdb")){
-    dbAppendTable(con, "afnewsdb", x)
-  }else{
-    dbCreateTable(con, "afnewsdb", x)
-  }
-  dbDisconnect(con)
-}else{print("Error")}
+print(con)
+dbDisconnect(con)
