0356485fcd8c49925e6029f8433e232d5ad66378
armelsoubeiga <armelsoubeiga93@gmail.com>
2024-02-15 12:41:48 +0100

    Update run.R

--- run.R
+++ run.R
@@ -28,7 +28,7 @@ x <- data.frame(col=rnorm(1:10))
 # Creation et enregistrement
 if(getcon){
   #db <- dbReadTable(con, "tlbafnews")
-  db <- dbGetQuery(con, 'SELECT * FROM "tlbafnews"')
+  db <- dbGetQuery(con, 'SELECT * FROM "tlbafnews" LIMIT 100')
   print(dim(db))
   dbDisconnect(con)
 }else{print("Error")}
